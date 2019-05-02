19a1be82969eb6a03bd519fb1e58c50250086dc0
Sander <sjlikers@gmail.com>
2019-05-02 11:53:17 +0200

    added grid distance checker

--- opengl/Mini_Game_KerasControl/Labyrinth_Generator.py
+++ opengl/Mini_Game_KerasControl/Labyrinth_Generator.py
@@ -1,14 +1,30 @@
 import numpy as np
 import random
+import math
 
 random.seed(420)
-a = np.empty([1])
+goals = np.empty([1])
 goal_paths = []
 avoidance_paths = []
 
 
-def check_shortest_distance(goal, origin):
-    pass
+def check_shortest_distance(p_target, p_origin=np.zeros([2])):
+    m = 0
+    target = p_target - p_origin
+
+    if target[0] == 0:
+        return abs(target[1])
+
+    elif target[1] == 0:
+        return abs(target[0])
+
+    elif target[0] / target[1] > 1:
+        m = target[1] / target[0]
+        return target[1] * math.ceil(m)
+
+    elif target[0] / target[1] <= 1:
+        m = target[0] / target[1]
+        return target[1] * math.ceil(m)
 
 
 def check_path_perimiter(point_list):
@@ -16,8 +32,9 @@ def check_path_perimiter(point_list):
 
 
 def generate_goals(amount=1):
+
     for x in range(amount):
-        goal = np.append(goal, [int(10 * random.random()), int(10 * random.random())])
+        goals = np.append(goals, [int(10 * random.random()), int(10 * random.random())])
 
 
 def generate_path_to_goal(goal, length, amount):
