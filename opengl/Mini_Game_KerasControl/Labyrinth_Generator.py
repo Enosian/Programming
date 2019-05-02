137808f56351fdba28afb37ee5f992d6915351e3
Sander <sjlikers@gmail.com>
2019-05-02 19:35:04 +0200

    added path distance calculator for grid

--- opengl/Mini_Game_KerasControl/Labyrinth_Generator.py
+++ opengl/Mini_Game_KerasControl/Labyrinth_Generator.py
@@ -18,13 +18,31 @@ def check_shortest_distance(p_target, p_origin=np.zeros([2])):
     elif target[1] == 0:
         return abs(target[0])
 
-    elif target[0] / target[1] > 1:
-        m = target[1] / target[0]
-        return target[1] * math.ceil(m)
-
-    elif target[0] / target[1] <= 1:
+    else:
+        # transform into mx+b function
         m = target[0] / target[1]
-        return target[1] * math.ceil(m)
+
+        def tempvar():
+            # intiger of the x value
+            t = math.floor(target[1])
+            return t
+
+        y = np.zeros([tempvar(), 10])
+        count = 0
+
+        for x in range(tempvar()):
+            for n in range(10):
+                y[x, n] = m * ((x * 10 + n) / 10)
+
+        for x in range(y.shape[0]):
+            for n in range(math.floor(target[0])):
+                print(n)
+                condition = False
+                for i in range(y.shape[1]):
+                    if not condition and y[x, i] < n + 1 and y[x, i] > n:
+                        condition = True
+                        count += 1
+        return(count)
 
 
 def check_path_perimiter(point_list):
@@ -34,7 +52,7 @@ def check_path_perimiter(point_list):
 def generate_goals(amount=1):
 
     for x in range(amount):
-        goals = np.append(goals, [int(10 * random.random()), int(10 * random.random())])
+        goals = np.append(goals, [math.floor(10 * random.random()), math.floor(10 * random.random())])
 
 
 def generate_path_to_goal(goal, length, amount):
@@ -51,3 +69,6 @@ def generate_point_matrix():
 
 def generate_labyrinth():
     pass
+
+
+print(check_shortest_distance(np.array([2, 3])))
