9869684adb6dcb9562ff1a3351726d455140c693
Sander <sjlikers@gmail.com>
2019-05-21 18:46:46 +0200

    added functionality

--- Konrads_fortnite_bot/Blind_bot.py
+++ Konrads_fortnite_bot/Blind_bot.py
@@ -1,12 +1,26 @@
-# import keyboard
+import keyboard
 # import mouse
 import time
 import random
 
 
-def ingame():
+def in_game():
+    end_time = time.time() + 9
+    while(time.time() < end_time):
+        keys = {0: "a", 1: "s", 2: "d"}
+        key = keys[random.randint(0, 2)]
+        duration = random.randint(0, 5)
+        # press key
+        keyboard.press(key)
+        time.wait(duration)
+
+
+def bus_game():
     pass
 
 
 def restart_game():
     pass
+
+
+in_game()
