2b917f564737ee6c12ea75501cadd1c165155c33
Sander <sjlikers@gmail.com>
2019-05-21 19:05:29 +0200

    cool

--- Konrads_fortnite_bot/Blind_bot.py
+++ Konrads_fortnite_bot/Blind_bot.py
@@ -5,14 +5,18 @@ import random
 
 
 def in_game():
-    end_time = time.time() + 9
+    end_time = time.time() + 900
     while(time.time() < end_time):
         keys = {0: "a", 1: "s", 2: "d"}
         key = keys[random.randint(0, 2)]
         duration = random.randint(0, 5)
         # press key
         keyboard.press(key)
-        time.wait(duration)
+        time.sleep(duration)
+        keyboard.release(key)
+        keyboard.press("w")
+        time.sleep(duration)
+        keyboard.release("w")
 
 
 def bus_game():
@@ -20,7 +24,6 @@ def bus_game():
 
 
 def restart_game():
-    pass
 
 
 in_game()
